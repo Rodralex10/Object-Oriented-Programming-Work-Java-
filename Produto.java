1bbe39e5e6052146185ddcd164ac531734c41cbb
Rodrigo Lourenço <104650956+Rodralex10@users.noreply.github.com>
2024-12-23 23:51:29 +0000

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,78 +1,130 @@
 package Trabalho;
 
 import java.io.Serializable;
+import java.util.*;
+import java.lang.*;
 
-public class Produto implements Serializable {
+public class Produto implements Serializable{
 
-    private int id;
-    private static int ultimo = 0;
-    private String Categoria;
-    private String NomeProduto;
-    private double preco; // Novo atributo para o preço do produto
-    private int quantidade; // Novo atributo para a quantidade do produto
 
-    // Construtor atualizado
-    public Produto(String Categoria, String NomeProduto, double preco, int quantidade) {
-        ultimo++;
-        id = ultimo;
-        this.Categoria = Categoria;
-        this.NomeProduto = NomeProduto;
-        this.preco = preco;
-        this.quantidade = quantidade;
-    }
+ private int id;
+ private static int ultimo = 0;
+ private String Categoria;
+ private String NomeProduto;
+ private double price;
+ private String Idadeindicada;
+ private String descricao;
+ private int stock;
+ 
+ public Produto(String Categoria, String NomeProduto, double price, String Idadeindicada, String descricao, int stock) {
+     ultimo++;
+     this.id = ultimo;
+     this.Categoria = Categoria;
+     this.NomeProduto = NomeProduto;
+     this.price = price;
+     this.Idadeindicada = Idadeindicada;
+     this.descricao = descricao;
+     this.stock = stock;
+ }
+ 
+ public String getDescricao() {
+	 return descricao;
+ }
+ 
+ public void setDescricao(String descricao) {
+	 this.descricao=descricao;
+ }
+ 
+ public int getId() {
+	 return id;
+ }
+ 
+ public static int getUltimo() { 
+		return ultimo; 
+		}
+ 
+ public double getPrice() {
+	 	return price;
+ 		}
+ 
+ public String getIdadeindicada() {
+	 return Idadeindicada;
+ }
+ 
+ public void setIdadeindicada(String Idadeindicada) {
+	 this.Idadeindicada = Idadeindicada;
+ }
+ 
+ public void setPrice(double price) {
+	 	this.price = price;
+ 		}
+	
+ public static void setUltimo(int ultimo) { 
+		Produto.ultimo = ultimo; 
+		}
+ 
+ public String getCategoria() {
+	 return Categoria;
+ }
+ 
+ public void setCategoria(String Categoria) {
+	 this.Categoria = Categoria;
+ }
+ 
+ public String getProduto() {
+	 return NomeProduto;
+ }
+ 
+ public void setProduto(String NomeProduto) {
+	 this.NomeProduto = NomeProduto;
+ }
+ 
+ public int getStock() {
+     return stock;
+ }
 
-    public int getId() {
-        return id;
-    }
+ public void setStock(int stock) {
+     this.stock = stock;
+ }
+ 
+ public void adicionarStock(int quantidade) {
+     this.stock += quantidade;
+ }
 
-    public static int getUltimo() {
-        return ultimo;
-    }
-
-    public static void setUltimo(int ultimo) {
-        Produto.ultimo = ultimo;
-    }
-
-    public String getCategoria() {
-        return Categoria;
-    }
-
-    public void setCategoria(String Categoria) {
-        this.Categoria = Categoria;
-    }
-
-    public String getProduto() {
-        return NomeProduto;
-    }
-
-    public void setProduto(String NomeProduto) {
-        this.NomeProduto = NomeProduto;
-    }
-
-    public double getPreco() { // Getter para o preço
-        return preco;
-    }
-
-    public void setPreco(double preco) { // Setter para o preço
-        this.preco = preco;
-    }
-
-    public int getQuantidade() { // Getter para a quantidade
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) { // Setter para a quantidade
-        this.quantidade = quantidade;
-    }
-
-    @Override
-    public String toString() {
-        return "Produto{" +
-                "id=" + id +
-                ", Categoria='" + Categoria + '\'' +
-                ", Produto='" + NomeProduto + '\'' +
-                ", Preço=" + preco +
-                ", Quantidade=" + quantidade +
-                '}';
-    }
+ public boolean retirarStock(int quantidade) {
+     if (quantidade <= stock) {
+         this.stock -= quantidade;
+         return true;
+     } else {
+         System.out.println("Stock insuficiente!");
+         return false;
+     }
+ }
+ 
+ public boolean temStock() {
+	 return stock > 0;
+ }
+ 
+ public boolean equals(Object obj) {
+	 if(obj != null && this.getClass()==obj.getClass()) {
+		 Produto li = (Produto) obj;
+		 return this.getCategoria().equals(li.getCategoria()) && 
+				 this.getDescricao().equals(li.getDescricao())&&
+				 this.getPrice()==li.getPrice()&&
+				 this.getIdadeindicada().equals(li.getIdadeindicada())&&
+				 this.getProduto().equals(li.getProduto())&&
+				 this.getId()==li.getId();
+	 }
+	 return false;
+ }
+ 
+ @Override
+ public String toString() {
+	 return "Produto{" + "id=" + id + 
+			 ", Categoria=" + Categoria + 
+			 ", Produto=" + NomeProduto + 
+			 ", Preço= "+ price + " €"+
+			 ", Idade Recomendada: "+ Idadeindicada +
+			 ", Descriçao: "+ descricao + '}';
+ }
 }
