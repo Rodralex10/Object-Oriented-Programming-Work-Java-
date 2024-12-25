0e0861b43ff5ca4a643e986744d8d403314153cf
Rodrigo Lourenço <104650956+Rodralex10@users.noreply.github.com>
2024-12-25 04:13:51 +0000

    Update FuncAuxiliares.java

--- FuncAuxiliares.java
+++ FuncAuxiliares.java
@@ -2,7 +2,9 @@ package Trabalho;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
 import myinputs.Ler;
+
 public class FuncAuxiliares {
 	
 	public static void ClearConsole() {
@@ -10,20 +12,6 @@ public class FuncAuxiliares {
 	            System.out.println();
 	        }
 	    }
-	
-	public static void createFile() {
-        try {
-            File file = new File("Loja.ser");
-            if (file.createNewFile()) {
-                System.out.println("File created: " + file.getName());
-            } else {
-                System.out.println("File already exists.");
-            }            
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
 	
 	public static int menuAdmin(){ // função na classe do main
 		 int opcao;
@@ -42,7 +30,7 @@ public class FuncAuxiliares {
 		 System.out.println("2 - Listar Produtos");
 		 System.out.println("3 - Apagar Produto");
 		 System.out.println("4 - Alterar Categoria de um produto");
-		 System.out.println("5 - Por Implementar");
+		 System.out.println("5 - Gerenciador de Stocks");
 		 System.out.println("6 – Por Implementar");
 		 System.out.println("7 - Sair");
 		 System.out.println("Qual a sua opção:");
@@ -75,29 +63,48 @@ public class FuncAuxiliares {
 		 return opcao;
 		 }
 	
-	public static int menuCategorias(){ // função na classe do main
+	public static int menuCarrinho(){ // função na classe do main
 		 int opcao;
-		 System.out.println("1 - Categoria 1");
-		 System.out.println("2 - Categoria 2");
-		 System.out.println("3 - Categoria 3");
-		 System.out.println("4 - Categoria 4");
-		 System.out.println("5 - Categoria 5");
-		 System.out.println("6 – Categoria 6");
+		 System.out.println("1 - Consultar Carrinho");
+		 System.out.println("2 - Tirar Produto do Carrinho");
+		 System.out.println("3 - Finalizar Compra");
+		 System.out.println("4 - Cancelar/Apagar Carrinho");
+		 System.out.println("5 - Sair");
+		 System.out.println("Qual a sua opção:");
+		 opcao = Ler.umInt();
+		 return opcao;
+	 	}
+	
+	public static int menuConsultarProdutos(){ // função na classe do main
+		 int opcao;
+		 System.out.println("1 - Listar Categorias");
+		 System.out.println("2 - Consultar Produto dado Nome");
+		 System.out.println("3 - Consultar Produtos de Categorias");
+		 System.out.println("4 - Consultar Produto dado ID");
+		 System.out.println("5 - Adicionar Produto ao Carrinho por ID");
+		 System.out.println("6 – Adicionar Produto ao Carrinho por Nome");
 		 System.out.println("7 - Sair");
 		 System.out.println("Qual a sua opção:");
 		 opcao = Ler.umInt();
 		 return opcao;
 	 	}
 	
-	public static int menuUser(){ // função na classe do main
+	public static int menuCliente(){ // função na classe do main
 		 int opcao;
-		 System.out.println("1 - Listar Categorias");
-		 System.out.println("2 - Consultar Produto dado Nome");
-		 System.out.println("3 - Consultar Produtos de Categorias");
-		 System.out.println("4 - Consultar Produto dado ID");
-		 System.out.println("5 - Comprar Produto por ID");
-		 System.out.println("6 – ");
-		 System.out.println("7 - Sair");
+		 System.out.println("1 - Pesquisar No Catalogo");
+		 System.out.println("2 - Carrinho de Compras");
+		 System.out.println("3 - Consultar Estatisticas Pessoais e Faturas");
+		 System.out.println("4 - Sair");
+		 System.out.println("Qual a sua opção:");
+		 opcao = Ler.umInt();
+		 return opcao;
+	 	}
+	public static int menuAdminStockManager(){ // função na classe do main
+		 int opcao;
+		 System.out.println("1 - Consultar Stock de Produtos");
+		 System.out.println("2 - Adicionar Stocks");
+		 System.out.println("3 - Remover Stocks");
+		 System.out.println("4 - Sair");
 		 System.out.println("Qual a sua opção:");
 		 opcao = Ler.umInt();
 		 return opcao;
