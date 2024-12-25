212c4af9250017856e370bc8f3d34200a8abc678
Rodrigo Lourenço <104650956+Rodralex10@users.noreply.github.com>
2024-12-25 04:13:21 +0000

    Update FuncLogin.java

--- FuncLogin.java
+++ FuncLogin.java
@@ -1,23 +1,26 @@
 package Trabalho;
+
 import java.io.FileOutputStream;
+
 import java.time.*;
 import java.time.format.*;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import myinputs.Ler;
+
 public class FuncLogin {
     private static ArrayList<Utilizador> Utilizadores = new ArrayList<>();
     
     public static void setUtilizadores(ArrayList<Utilizador> lista) {
         Utilizadores = lista;
     }
+
     // Método para obter a lista de utilizadores
     public static ArrayList<Utilizador> getUtilizadores() {
         return Utilizadores;
     }
-    LocalDateTime localDate = LocalDateTime.now();
-    DateTimeFormatter dateformatter = DateTimeFormatter.ofPattern("MM dd, YYYY");
+ 
     // Função para registrar um novo utilizador
     public static void register() {
         String Username, Password, ConfirmPass;
@@ -30,6 +33,7 @@ public class FuncLogin {
         
         System.out.println("\nConfirm Password: ");
         ConfirmPass = Ler.umaString();
+
         // Verificar se as senhas coincidem
         if (!Password.equals(ConfirmPass)) {
             System.out.println("Passwords não coincidem. Tente novamente.");
@@ -40,27 +44,33 @@ public class FuncLogin {
                 return;
             }
         }
+
         // Registrar um novo utilizador
         Cliente novoCliente = new Cliente(Username, Password);
         Utilizadores.add(novoCliente);
         atualizarFicheiro();
         System.out.println("Utilizador registrado com sucesso!");
     }
+
     // Função para fazer login
     public static Utilizador login() {
+    	LocalDate today = LocalDate.now();
         String Username, Password;
-           
+    
         if (Utilizadores.isEmpty()) {
             Administrador admin = new Administrador("admin", "admin", "Admin");
             Utilizadores.add(admin);
         }
+
         System.out.println("Username:  ");
         Username = Ler.umaString();
+
         System.out.println("\nPassword:  ");
         Password = Ler.umaString();
+
         for (Utilizador u : Utilizadores) {
             if (u.getUsername().equals(Username) && u.getPassword().equals(Password)) {
-                System.out.println("Login efetuado com sucesso! Bem-vindo, " + Username + " são " );
+                System.out.println("Login efetuado com sucesso! Bem-vindo, " + Username + " são " + today);
                 return u;
             }
         }
@@ -70,7 +80,9 @@ public class FuncLogin {
     
     public static void removerUtilizador() {
         System.out.println("Qual o o Username da conta? ");
+
         String username = Ler.umaString();
+
         // Validar número de Produto e remover o Produto
         for (int i = 0; i < Utilizadores.size(); i++) {
             if (Utilizadores.get(i).getUsername().equals(username)) {
@@ -102,6 +114,7 @@ public class FuncLogin {
         
         System.out.println("\nConfirm Password: ");
         ConfirmPass = Ler.umaString();
+
         // Verificar se as senhas coincidem
         if (!Password.equals(ConfirmPass)) {
             System.out.println("Passwords não coincidem. Tente novamente.");
@@ -117,10 +130,12 @@ public class FuncLogin {
         atualizarFicheiro();
         System.out.println("Utilizador registrado com sucesso!");
     }
+
     // Atualizar o ficheiro Loja.dat com os Utilizadores registrados
     private static void atualizarFicheiro() {
         try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("Utilizadores.ser"))) {
-            os.writeObject(Utilizadores);
+            os.writeInt(Utilizador.getUltimo()); // Salvar o último ID de utilizador
+            os.writeObject(Utilizadores); // Salvar a lista de utilizadores
             System.out.println("Ficheiro de utilizadores atualizado com sucesso!");
         } catch (IOException e) {
             System.out.println("Erro ao atualizar o ficheiro: " + e.getMessage());
