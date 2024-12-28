fe22bfe4f15a66334bda230a02c5620425e809d8
Rodrigo Lourenço <104650956+Rodralex10@users.noreply.github.com>
2024-12-28 03:32:32 +0000

    Update FuncAuxiliares.java

--- Trabalho/FuncAuxiliares.java
+++ Trabalho/FuncAuxiliares.java
@@ -16,9 +16,9 @@ public class FuncAuxiliares {
 	public static int menuAdmin(){ // Menu for the admin to choose main options
 		 int opcao;
 		 System.out.println("/====================================================/");
-	     System.out.println("/                 MENU ADMINISTRADOR                 /");
-	     System.out.println("/====================================================/");
-	     System.out.println("/====================================================/");
+	         System.out.println("/                 MENU ADMINISTRADOR                 /");
+	         System.out.println("/====================================================/");
+	     	 System.out.println("/====================================================/");
 		 System.out.println("/      [1] - Gestão de Utilizadores                  /");
 		 System.out.println("/      [2] - Gestão de Loja/Stocks                   /");
 		 System.out.println("/      [3] - Estatísticas da Loja                    /");
@@ -32,9 +32,9 @@ public class FuncAuxiliares {
 	public static int menuAdminGestao(){ // Menu for admin's product management options
 		 int opcao;
 		 System.out.println("/====================================================/");
-	     System.out.println("/                GESTÃO DE LOJA/STOCKS               /");
-	     System.out.println("/====================================================/");
-	     System.out.println("/====================================================/");
+	         System.out.println("/                GESTÃO DE LOJA/STOCKS               /");
+	         System.out.println("/====================================================/");
+	         System.out.println("/====================================================/");
 		 System.out.println("/      [1] - Novo Produto                            /");
 		 System.out.println("/      [2] - Listar Produtos                         /");
 		 System.out.println("/      [3] - Apagar Produto                          /");
@@ -50,9 +50,9 @@ public class FuncAuxiliares {
 	public static int menuAdminUserManager(){ // Menu for admin's user management options
 		 int opcao;
 		 System.out.println("/====================================================/");
-	     System.out.println("/                GESTÃO DE UTILIZADORES              /");
-	     System.out.println("/====================================================/");
-	     System.out.println("/====================================================/");
+	         System.out.println("/                GESTÃO DE UTILIZADORES              /");
+	         System.out.println("/====================================================/");
+	         System.out.println("/====================================================/");
 		 System.out.println("/      [1] - Apagar Utilizador                       /");
 		 System.out.println("/      [2] - Listar Utilizadores                     /");
 		 System.out.println("/      [3] – Criar Novo Admin User                   /");
@@ -66,9 +66,9 @@ public class FuncAuxiliares {
 	public static int menuAdminShopPerformance(){ //  Menu for admin's shop performance analytics
 		 int opcao;
 		 System.out.println("/====================================================/");
-	     System.out.println("/                ESTATÍSTICA DA LOJA                 /");
-	     System.out.println("/====================================================/");
-	     System.out.println("/====================================================/");
+	         System.out.println("/                ESTATÍSTICA DA LOJA                 /");
+	         System.out.println("/====================================================/");
+	         System.out.println("/====================================================/");
 		 System.out.println("/      [1] - Produtos Mais e Menos Vendidos          /");
 		 System.out.println("/      [2] - Dois Melhores Clientes                  /");
 		 System.out.println("/      [3] - Valor Faturado no dia de hoje           /");
@@ -86,9 +86,9 @@ public class FuncAuxiliares {
 	public static int menuCarrinho(){ // Menu for cart management options
 		 int opcao;
 		 System.out.println("/====================================================/");
-	     System.out.println("/                CARRINHO DE COMPRAS                 /");
-	     System.out.println("/====================================================/");
-	     System.out.println("/====================================================/");
+	         System.out.println("/                CARRINHO DE COMPRAS                 /");
+	         System.out.println("/====================================================/");
+	         System.out.println("/====================================================/");
 		 System.out.println("/      [1] - Consultar Carrinho                      /");
 		 System.out.println("/      [2] - Tirar Produto do Carrinho               /");
 		 System.out.println("/      [3] - Finalizar Compra                        /");
@@ -103,9 +103,9 @@ public class FuncAuxiliares {
 	public static int menuConsultarProdutos(){ // Menu for product consultation options
 		 int opcao;
 		 System.out.println("/====================================================/");
-	     System.out.println("/                PESQUISAR NO CATÁLOGO               /");
-	     System.out.println("/====================================================/");
-	     System.out.println("/====================================================/");
+	         System.out.println("/                PESQUISAR NO CATÁLOGO               /");
+	         System.out.println("/====================================================/");
+	         System.out.println("/====================================================/");
 		 System.out.println("/      [1] - Listar Categorias                       /");
 		 System.out.println("/      [2] - Consultar Produto dado Nome             /");
 		 System.out.println("/      [3] - Consultar Produtos de Categorias        /");
@@ -123,9 +123,9 @@ public class FuncAuxiliares {
 	public static int menuCliente(){ // Menu for customer options
 		 int opcao;
 		 System.out.println("/====================================================/");
-	     System.out.println("/                   MENU UTILIZADOR                  /");
-	     System.out.println("/====================================================/");
-	     System.out.println("/====================================================/");
+	         System.out.println("/                   MENU UTILIZADOR                  /");
+	         System.out.println("/====================================================/");
+	         System.out.println("/====================================================/");
 		 System.out.println("/      [1] - Pesquisar No Catálogo                   /");
 		 System.out.println("/      [2] - Carrinho de Compras                     /");
 		 System.out.println("/      [3] - Consultar Faturas                       /");
@@ -138,9 +138,9 @@ public class FuncAuxiliares {
 	public static int menuAdminStockManager(){ // Menu for admin's stock management options
 		 int opcao;
 		 System.out.println("/====================================================/");
-	     System.out.println("/                GERENCIADOR DE STOCKS               /");
-	     System.out.println("/====================================================/");
-	     System.out.println("/====================================================/");
+	         System.out.println("/                GERENCIADOR DE STOCKS               /");
+	         System.out.println("/====================================================/");
+	         System.out.println("/====================================================/");
 		 System.out.println("/      [1] - Consultar Stock de Produtos             /");
 		 System.out.println("/      [2] - Adicionar Stocks                        /");
 		 System.out.println("/      [3] - Remover Stocks                          /");
